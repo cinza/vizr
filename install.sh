36e7e3cdf96d0a24edc59d0fc58619b27a6a05c1
Howard Rauscher <howardr@gmail.com>
2011-10-05 15:30:19 -0500

    fix for install script

--- install.sh
+++ install.sh
@@ -3,15 +3,12 @@
 name="vizr"
 app_path=~/code/${name}
 
-echo "creating directory ${app_path}"
-mkdir -p ${app_path} &> ${app_path}/install.log
-
 echo "creating or updating app"
-git clone git@github.com:howardr/vizr.git ${app_path} &> ${app_path}/install.log
+git clone git@github.com:howardr/vizr.git ${app_path}
 cd ${app_path}
-git pull &> ${app_path}/install.log
+git pull
 
 echo "installing dependencies and app"
-rake install &> ${app_path}/install.log
+rake install
 
 echo "complete"
