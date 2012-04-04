b299eee395a2ff9e5d203c73847bc6dd7975c240
Troy Warr <troy3000@gmail.com>
2012-04-04 12:42:29 -0500

    check for main.css and main.js before attempting to merge

--- tasks/build.rb
+++ tasks/build.rb
@@ -27,25 +27,29 @@ task :process_files, :target, :options do |t, args|
   minify = options[:minify]
   jslint = options[:jslint]
   jopts = ''
-  
+
   dev = File.expand_path(DEV_PATH, target)
   tmp = File.expand_path(TMP_PATH, target)
   build = File.expand_path(BUILD_PATH, target)
-  
+
   if !minify
     jopts << ' -m none'
   end
-  
+
   if !jslint
     jopts << ' -s'
   end
 
-  sh "juicer merge -i #{jopts} #{File.join(tmp, "css", "main.build.css")} #{File.join(tmp, "css", "main.css")}"
-  mv(File.join(tmp, "css", "main.min.css"), File.join(tmp, "css", "main.css"), :force => true)
-  
-  sh "juicer merge -i #{jopts} none #{File.join(tmp, "css", "main.build.js")} #{File.join(tmp, "js", "main.js")}"
-  mv(File.join(tmp, "js", "main.min.js"), File.join(tmp, "js", "main.js"), :force => true)
-  
+  if File.exist?("main.css")
+    sh "juicer merge -i #{jopts} #{File.join(tmp, "css", "main.build.css")} #{File.join(tmp, "css", "main.css")}"
+    mv(File.join(tmp, "css", "main.min.css"), File.join(tmp, "css", "main.css"), :force => true)
+  end
+
+  if File.exist?("main.js")
+    sh "juicer merge -i #{jopts} none #{File.join(tmp, "css", "main.build.js")} #{File.join(tmp, "js", "main.js")}"
+    mv(File.join(tmp, "js", "main.min.js"), File.join(tmp, "js", "main.js"), :force => true)
+  end
+
   Dir[File.join(tmp, "*.hbs")].each do |hbs|
     sh "ruby #{File.join(VIZR_ROOT, "lib", "parse_hbs.rb")} \"#{hbs}\" #{File.join(target, "env.yaml")} > #{File.join(tmp, File.basename(hbs, ".hbs"))}"
   end
@@ -71,9 +75,9 @@ task :move_to_build, :target do |t, args|
     if File.exists?(File.join(tmp, folder))
       mkdir_p(File.join(build, folder))
       cp_r(File.join(tmp, folder), build)
-    end 
+    end
   end
-  
+
   cp(Dir[File.join(tmp, "*.html")], build)
   cp(Dir[File.join(tmp, "*.manifest")], build)
 end
