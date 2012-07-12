4d5cd10d9ac8ea2244f70b8362142d6a3ee1b3b1
Michael Pazienza <michael.pazienza@encryptocat.com>
2012-07-12 16:57:42 -0500

    Changed pretty date to support the concise format

--- projects/package/dev/templates/helpers/prettyDate.js
+++ projects/package/dev/templates/helpers/prettyDate.js
@@ -7,59 +7,77 @@ define(['vendor/handlebars', 'vendor/massrel'], function(Handlebars, massrel) {
    */
 
   // takes an ISO time and returns a string representing how long ago the date represents
-  function prettyDate(time) {
-
+  function prettyDate(time, format) {
     var date = new Date((time || '').replace(/-/g, '/').replace(/[TZ][^hu]/g, ' ')),
         diff = (((new Date()).getTime() - date.getTime()) / 1000),
-        day_diff = Math.floor(diff / 86400),
-        mth_diff = (((new Date()).getFullYear() - date.getFullYear()) * 12) + ((new Date()).getMonth() + 1) - (date.getMonth() + 1),
-        // string settings
-        time_str = {
-          just_now: 'a moment ago',
-          one_minute_ago: '1 min ago',
-          minutes_ago: 'mins ago',
-          one_hour_ago: '1 hour ago',
-          hours_ago: 'hours ago',
-          yesterday: 'yesterday',
-          days_ago: 'days ago',
-          one_week_ago: '1 week ago',
-          weeks_ago: 'weeks ago',
-          one_month_ago: 'last month',
-          months_ago: 'months ago',
-          one_year_ago: 'last year',
-          years_ago: 'years ago'
-        };
-
-    if (isNaN(day_diff) || day_diff < 0) {
-      return time_str.just_now;
+        dayDiff = Math.floor(diff / 86400),
+        mthDiff = (((new Date()).getFullYear() - date.getFullYear()) * 12) + ((new Date()).getMonth() + 1) - (date.getMonth() + 1),
+        // time format options
+        timeFormat = {
+          basic: {
+            justNow: 'a moment ago',
+            oneMinuteAgo: '1 min ago',
+            minutesAgo: ' mins ago',
+            oneHourAgo: '1 hour ago',
+            hoursAgo: ' hours ago',
+            yesterday: 'yesterday',
+            daysAgo: ' days ago',
+            oneWeekAgo: '1 week ago',
+            weeksAgo: ' weeks ago',
+            oneMonthAgo: 'last month',
+            monthsAgo: 'months ago',
+            oneYearAgo: 'last year',
+            yearsAgo: 'years ago'
+          },
+          concise: {
+            justNow: 'now',
+            oneMinuteAgo: '1m',
+            minutesAgo: 'm',
+            oneHourAgo: '1h',
+            hoursAgo: 'h',
+            yesterday: '1d',
+            daysAgo: 'd',
+            oneWeekAgo: '1w',
+            weeksAgo: 'w',
+            oneMonthAgo: '1m',
+            monthsAgo: 'm',
+            oneYearAgo: '1y',
+            yearsAgo: 'y'
+          }
+        },
+        timeStr = timeFormat[format || 'basic'];
+  
+    if (isNaN(dayDiff) || dayDiff < 0) {
+      return timeStr.justNow;
+    } else if (dayDiff >= 31) {
+      return;
     }
-
-    return (day_diff === 0) && (
-      (diff < 60 && time_str.just_now) ||
-      (diff < 120 && time_str.one_minute_ago) ||
-      (diff < 3600 && Math.floor( diff / 60 ) + ' ' + time_str.minutes_ago) ||
-      (diff < 7200 && time_str.one_hour_ago) ||
-      (diff < 86400 && Math.floor( diff / 3600 ) + ' ' + time_str.hours_ago)
-    ) || (mth_diff === 0) && (
-      (day_diff === 1 && time_str.yesterday) ||
-      (day_diff < 7 && day_diff + ' ' + time_str.days_ago) ||
-      (day_diff < 14 && time_str.one_week_ago) ||
-      (day_diff < 31 && Math.ceil( day_diff / 7 ) + ' ' + time_str.weeks_ago)
+  
+    return (dayDiff === 0) && (
+      (diff < 60 && timeStr.justNow) ||
+      (diff < 120 && timeStr.oneMinuteAgo) ||
+      (diff < 3600 && Math.floor( diff / 60 ) + timeStr.minutesAgo) ||
+      (diff < 7200 && timeStr.oneHourAgo) ||
+      (diff < 86400 && Math.floor( diff / 3600 ) + timeStr.hoursAgo)
+    ) || (mthDiff === 0) && (
+      (dayDiff === 1 && timeStr.yesterday) ||
+      (dayDiff < 7 && dayDiff + timeStr.daysAgo) ||
+      (dayDiff < 14 && timeStr.oneWeekAgo) ||
+      (dayDiff < 31 && Math.ceil( dayDiff / 7 ) + timeStr.weeksAgo)
     ) ||
-    (mth_diff === 1 && time_str.one_month_ago) ||
-    (mth_diff < 12 && mth_diff + ' ' + time_str.months_ago) ||
-    (mth_diff === 12 && time_str.one_year_ago) ||
-    (Math.floor(mth_diff / 12) + ' ' + time_str.years_ago);
-
+    (mthDiff === 1 && timeStr.oneMonthAgo) ||
+    (mthDiff < 12 && mthDiff + timeStr.monthsAgo) ||
+    (mthDiff === 12 && timeStr.oneYearAgo) ||
+    (Math.floor(mthDiff / 12) + timeStr.yearsAgo);
+  
   }
 
-  function prettyTwitterDate(date) {
+  function prettyTwitterDate(date, format) {
     date = massrel.helpers.fix_twitter_date(date);
-    return prettyDate(date);
+    return prettyDate(date, format);
   }
 
   Handlebars.registerHelper('prettyDate', prettyTwitterDate);
 
   return prettyTwitterDate;
 });
-
