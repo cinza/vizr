72dc0c9118fc82416658e1a8897b711f0eebc3cc
Howard Rauscher <howardr@gmail.com>
2013-03-06 15:24:50 -0600

    update analytics module to support basic fb events

--- projects/requirejs/dev/js/vendor/analytics.js
+++ projects/requirejs/dev/js/vendor/analytics.js
@@ -14,6 +14,39 @@ define(['vendor/jquery', 'vendor/twitter-widgets'], function($) {
     };
   }
 
+  function trackEvent() {
+    var args = [].slice.call(arguments);
+    args.unshift('_trackEvent');
+    gaPush(args);
+  }
+
+
+  // FB events
+  $('body').bind('fb:ready', function() {
+    if(window.FB && FB.Event) {
+      var likeToAnalytics = function(url) {
+        trackEvent('fbintent', 'like', url);
+      };
+      var unlikeToAnalytics = function(url) {
+        trackEvent('fbintent', 'unlike', url);
+      };
+
+      var sessionToAnalytics = function(name) {
+        return function() {
+          trackEvent('fbintent', 'auth', name);
+        };
+      };
+
+      FB.Event.subscribe('edge.create', likeToAnalytics);
+      FB.Event.subscribe('edge.remove', unlikeToAnalytics);
+      FB.Event.subscribe('auth.login', sessionToAnalytics('login'));
+      FB.Event.subscribe('auth.authResponseChange', sessionToAnalytics('authResponseChange'));
+      FB.Event.subscribe('auth.statusChange', sessionToAnalytics('statusChange'));
+      FB.Event.subscribe('auth.logout', sessionToAnalytics('logout'));
+      FB.Event.subscribe('auth.prompt', sessionToAnalytics('prompt'));
+    }
+  });
+
   if(window.twttr) {
     // modified from from
     // https://dev.twitter.com/docs/intents/events
@@ -21,14 +54,14 @@ define(['vendor/jquery', 'vendor/twitter-widgets'], function($) {
       var clickEventToAnalytics = function(intentEvent) {
         if(intentEvent) {
           var label = intentEvent.region;
-          gaPush(['_trackEvent', 'twitterintent', intentEvent.type, label]);
+          trackEvent('twitterintent', intentEvent.type, label);
         };
       };
      
       var tweetIntentToAnalytics = function(intentEvent) {
         if(intentEvent) {
           var label = 'tweet';
-          gaPush(['_trackEvent', 'twitterintent', intentEvent.type, label]);
+          trackEvent('twitterintent', intentEvent.type, label);
         };
       };
      
@@ -39,14 +72,14 @@ define(['vendor/jquery', 'vendor/twitter-widgets'], function($) {
       var retweetIntentToAnalytics = function(intentEvent) {
         if(intentEvent) {
           var label = intentEvent.data.source_tweet_id;
-          gaPush(['_trackEvent', 'twitterintent', intentEvent.type, label]);
+          trackEvent('twitterintent', intentEvent.type, label);
         };
       };
      
       var followIntentToAnalytics = function(intentEvent) {
         if(intentEvent) {
           var label = intentEvent.data.user_id + ' (' + intentEvent.data.screen_name + ')';
-          gaPush(['_trackEvent', 'twitterintent', intentEvent.type, label]);
+          trackEvent('twitterintent', intentEvent.type, label);
         };
       };
 
@@ -63,6 +96,7 @@ define(['vendor/jquery', 'vendor/twitter-widgets'], function($) {
     var link = $(this),
         href = link.attr('href'),
         target = link.closest('[data-massrel-id]'),
+        media = link.closest('.massrel-media').length > 0,
         status_id = target.attr('data-massrel-id'),
         network = target.closest('[data-massrel-network]').attr('data-massrel-network'),
         region = target.closest('[data-massrel-region]').attr('data-massrel-region');
@@ -73,8 +107,10 @@ define(['vendor/jquery', 'vendor/twitter-widgets'], function($) {
 
     if(href) {
       if(href.indexOf('twitter.com') > -1) {
-
-        if(href.indexOf('/status/') > -1) { // permalink
+        if(media) {
+          name = 'media';
+        }
+        else if(href.indexOf('/status/') > -1) { // permalink
           name = 'permalink';
         }
         else if(href.indexOf('/search?q=') > -1) { // hashtag
@@ -101,16 +137,36 @@ define(['vendor/jquery', 'vendor/twitter-widgets'], function($) {
           name = 'author'; // most likely author
         }
       }
+      else if(href.indexOf('facebook.com') > -1) {
+        if(media) {
+          name = 'media';
+        }
+        else if(href.indexOf('/likes') > -1) {
+          name = 'like';
+        }
+        else if(href.indexOf('/comments') > -1) {
+          name = 'comment';
+        }
+        else if(href.indexOf('/posts/') > -1) {
+          name = 'permalink';
+        }
+        else {
+          name = 'author';
+        }
+      }
       else {
         // status link
         name = 'link';
       }
 
       if(name) {
-        gaPush(['_trackEvent', 'tweetaction', name, region || 'other']);
+        trackEvent(network === 'twt' ? 'tweetaction' : network+'action', name, region || 'other');
       }
-
     }
   });
+
+  return {
+    trackEvent: trackEvent
+  };
 
 });
